67bb1a3930ec7efeb5baaf8f85b3703302cc46bc
robinsch <robinsch@users.noreply.github.com>
2021-05-23 18:04:31 +0200

    Update src/CFBG_SC.cpp
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- src/CFBG_SC.cpp
+++ src/CFBG_SC.cpp
@@ -60,7 +60,9 @@ public:
         sCFBG->FitPlayerInTeam(player, true, bg);
 
         if (sCFBG->IsEnableResetCooldowns())
+        {
             player->RemoveArenaSpellCooldowns(true);
+        }
     }
 
     void OnBattlegroundEndReward(Battleground* bg, Player* player, TeamId /*winnerTeamId*/) override
